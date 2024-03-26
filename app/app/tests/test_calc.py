ed3b6ff3c9e031c4ff3e572ea691aaa0080cfad6
irawild <irawildjunior@gmail.com>
2024-03-26 16:51:19 -0300

    Added unit tests

---
+++ app/app/tests/test_calc.py
@@ -0,0 +1,17 @@
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+    
+    def test_add_numbers(self):
+        """Test adding numbers."""
+        res = calc.add(1, 2)
+        self.assertEqual(res, 3)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
+        
+        self.assertEqual(res, 5)
